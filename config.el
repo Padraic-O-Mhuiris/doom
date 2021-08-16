9e28d4bf92978431d315689c64975bc5ac00ee1c
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-16 16:56:27 +0100

    first commit

---
+++ config.el
@@ -0,0 +1,47 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Patrick Morris"
+      user-mail-address "patrick.morris.310@gmail.com")
+
+(setq display-line-numbers-type t)
+
+(setq doom-font (font-spec :family "Iosevka" :size 14 :slant 'normal :weight 'normal))
+(setq doom-big-font (font-spec :family "Iosevka" :size 24))
+
+(setq doom-theme 'doom-moonlight)
+
+(setq auto-save-default t)
+
+(setq haskell-stylish-on-save t)
+
+(setq tab-width 2)
+(setq standard-indent 2)
+
+;;(setq org-todo-keywords
+;;      '((sequence "TODO" "FEEDBACK" "VERIFY" "|" "DONE" "DELEGATED")))
+
+;; Javascript settings
+(setq-hook! js2-mode-hook js-indent-level 2)
+(setq-hook! typescript-mode-hook typescript-indent-level 2)
+(setq-hook! typescript-tsx-mode-hook web-mode-code-indent-offset 2)
+;; Org
+(setq org-directory "~/.org")
+(setq org-roam-directory "~/.org")
+(setq org-roam-db-location "~/.org/org-roam.db")
+(setq org-roam-v2-ack t)
+(setq org-agenda-files '("~/.org"))
+
+(setq org-archive-location "~/.org/archive/%s_archive::* Archived Tasks")
+
+(setq org-download-image-dir "~/.org/images")
+
+(setq ledger-post-amount-alignment-column 100)
+(setq ledger-post-account-alignment-column 2)
+
+(setq all-the-icons-scale-factor 1.1)
---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/.org/people/oisín-kyne.org" "/home/padraic/.org/agenda/reminders.org" "/home/padraic/.org/agenda/health.org" "/home/padraic/.org/projects/nixos_system.org" "/home/padraic/.org/projects/org_roam_system.org" "/home/padraic/.org/agenda/house.org" "/home/padraic/.org/projects/ethereum_node.org" "/home/padraic/.org/projects/gorthaganny_community_website.org" "/home/padraic/.org/index.org" "/home/padraic/.org/agenda/finances.org" "/home/padraic/.org/daily/2021-07-17.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
