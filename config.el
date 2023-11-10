a5ea6a83f9ef7f142c00c479f814f7210556246e
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2023-11-10 16:28:20 +0000

    Update

--- config.el
+++ config.el
@@ -1,62 +1,9 @@
 (setq user-full-name "Patrick Morris"
       user-mail-address "patrick.morris.310@gmail.com")
 
-(setq doom-font (font-spec :family "Iosevka NFM" :size 24)
-      doom-big-font (font-spec :family "Iosevka NFM" :size 38)
-      doom-unicode-font (font-spec :family "Iosevka NFM")
-      doom-variable-pitch-font (font-spec :family "Iosevka NFM"))
+(setq doom-font (font-spec :family "Iosevka Comfy Fixed" :size 24)
+      doom-big-font (font-spec :family "Iosevka Comfy Fixed" :size 38)
+      doom-unicode-font (font-spec :family "Iosevka Comfy Fixed")
+      doom-variable-pitch-font (font-spec :family "Iosevka Comfy Fixed"))
 
-(setq doom-theme 'doom-nord-light)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-(after! org
-  (setq org-directory "~/notes")
-  (setq org-roam-directory "~/notes")
-  (setq org-agenda-files (directory-files-recursively "~/notes/" "\\.org$"))
-  (setq org-roam-dailies-directory "/dailies")
-  (setq time-stamp-active t)
-  (setq time-stamp-start "#\\+LAST_MODIFIED:[ \t]")
-  (setq time-stamp-end "$")
-  (setq time-stamp-format "\[%Y-%m-%d %a %H:%M:%S\]")
-
-  (add-hook 'before-save-hook 'time-stamp nil))
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-;;(setq org-directory "~/org/")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(setq doom-theme 'doom-nord-aurora)
